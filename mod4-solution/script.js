92665323357ca1fc26c63f374dc55e89e058c561
Helcio Candido <helcio.mcandido@gmail.com>
2022-03-21 00:03:24 -0400

    Module 4 - Coding Assignment Solution

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -13,11 +13,10 @@
   ];
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
-    window.name = names[i];
     if (firstLetter === "j") {
-      byeSpeaker.speak();
+      byeSpeaker.speak(names[i]);
     } else {
-      helloSpeaker.speak();
+      helloSpeaker.speak(names[i]);
     }
   }
 })();
